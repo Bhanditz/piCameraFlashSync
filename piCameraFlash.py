0fe1e0d28d99cd863df99db59862610bffd7f204
alvarohurtadobo <a.hurtado.bo@gmail.com>
2018-08-28 17:46:48 -0400

    Added simple preview

--- piCameraFlash.py
+++ piCameraFlash.py
@@ -1,16 +1,29 @@
 import os
 import sys
+import cv2
 import time
 import picamera
 
+rutaDeGuardado = './'
+
 if __name__ == '__main__':
     camera = picamera.PiCamera()
+    camera.resolution = (2592, 1944)
     camera.framerate = 2
     camera.exposure_mode = 'sports'
     camera.flash_mode = 'on'
-
+    piCameraStream = camera.capture_continuous(format="bgra",
+                                                use_video_port=True)
     camera.start_preview()
-    for i in range(5):
-        time.sleep(5)
-        camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+    contador = 0
+    while True:
+        lrs = piCameraStream.__next__()
+        imageArray = lrs.array
+        cv2.imshow('Imagen',v2.resize(imageArray,(320,240)))
+        cv2.imwrite(rutaDeGuardado+'imagen_{}.jpg'.format(contador), imageArray)
+        contador += 1
+        ch = 0xFF & waitKey(1)
+        if ch == ord('q'):
+            break
+
     camera.stop_preview()
