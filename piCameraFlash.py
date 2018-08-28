4c9714518c7c3b57a58734b3bfdd55d6b5a77f8e
alvarohurtadobo <a.hurtado.bo@gmail.com>
2018-08-28 17:27:46 -0400

    Added simple preview

--- piCameraFlash.py
+++ piCameraFlash.py
@@ -1,2 +1,16 @@
 import os
 import sys
+
+import picamera
+
+if __name__ == '__main__':
+    camera = picamera.PiCamera()
+    camera.framerate = 2
+    camera.exposure_mode = 'sports'
+    camera.flash_mode = 'on'
+
+    camera.start_preview()
+    for i in range(5):
+        sleep(5)
+        camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+    camera.stop_preview()
