5adc38740bfbc82f821e068242518217323229fc
alvarohurtadobo <a.hurtado.bo@gmail.com>
2018-08-29 10:22:26 -0400

    Moved parameters to init

--- piCameraFlash.py
+++ piCameraFlash.py
@@ -10,7 +10,7 @@ rutaDeGuardado = './'
 if __name__ == '__main__':
     # We initialize some parameters within the initializer so It has better performance
     camera = picamera.PiCamera(resolution = (2592,1944),framerate = 2,sensor_mode=0,clock_mode='reset')
-    camera.exposure_mode = 'sports'
+    #camera.exposure_mode = 'sports'
     #camera.flash_mode = 'on'
     lowResCap = PiRGBAArray(camera)
     piCameraStream = camera.capture_continuous(lowResCap,
@@ -19,7 +19,7 @@ if __name__ == '__main__':
                                                 use_video_port=True)
 
     contador = 0
-    #camera.start_preview()
+    camera.start_preview()
     while True:
         if contador == 1:
             print(camera.sensor_mode)
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         imageArray = lrs.array
         lowResCap.truncate(0)
         cv2.imshow('Imagen',cv2.resize(imageArray,(320,240)))
-        cv2.imwrite(rutaDeGuardado+'imagen_{}.jpg'.format(contador), imageArray)
+        #cv2.imwrite(rutaDeGuardado+'imagen_{}.jpg'.format(contador), imageArray)
         contador += 1
         ch = 0xFF & cv2.waitKey(1)
         if ch == ord('q'):
