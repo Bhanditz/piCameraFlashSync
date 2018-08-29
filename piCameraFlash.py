c4e3a5f9dbf59e3087b57f5c94ea2a8c9b9afc40
alvarohurtadobo <a.hurtado.bo@gmail.com>
2018-08-29 13:05:07 -0400

    Moved parameters to init

--- piCameraFlash.py
+++ piCameraFlash.py
@@ -38,6 +38,7 @@ if __name__ == '__main__':
 
                 camera.capture(output, 'rgb')
                 print('Captured %dx%d image' % (output.array.shape[1], output.array.shape[0]))
+                output.truncate(0)
                 print(type(output.array))
                 #cv2.imshow('Imagen',cv2.resize(imageArray,(320,240)))
                 #cv2.imwrite(rutaDeGuardado+'imagen_{}.jpg'.format(contador), imageArray)
